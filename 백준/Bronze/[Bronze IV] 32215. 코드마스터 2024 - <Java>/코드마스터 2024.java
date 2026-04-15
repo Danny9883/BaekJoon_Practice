2ea651ce41eacb523f96eca2ddb939237d6ce068
DANNY <danny9883@naver.com>
2026-04-15 09:07:52 +0900

    [Bronze IV] Title: 코드마스터 2024, Time: 160 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/[Bronze IV] 32215. 코드마스터 2024 - <Java>/코드마스터 2024.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader   br   = new BufferedReader(new InputStreamReader(System.in));
+		Scanner          sc   = new Scanner(System.in);
+		
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int k = sc.nextInt();
+		
+		System.out.println(m*k+m);
+		
+	
+
+		
+	
+		
+		br.close();
+			
+		
+			
+			
+	}
+
+
+}
