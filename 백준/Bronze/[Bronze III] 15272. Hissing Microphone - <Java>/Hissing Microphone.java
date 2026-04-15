d2562a31ea6502b7eac5293f9bb3abad5a6b3c70
DANNY <danny9883@naver.com>
2026-04-15 09:10:21 +0900

    [Bronze III] Title: Hissing Microphone, Time: 164 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/[Bronze III] 15272. Hissing Microphone - <Java>/Hissing Microphone.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader   br   = new BufferedReader(new InputStreamReader(System.in));
+		Scanner          sc   = new Scanner(System.in);
+		
+		
+		if (sc.nextLine().contains("ss"))
+			System.out.println("hiss");
+		else
+			System.out.println("no hiss");
+		
+	
+
+		
+	
+		
+		br.close();
+			
+		
+			
+			
+	}
+
+
+}
