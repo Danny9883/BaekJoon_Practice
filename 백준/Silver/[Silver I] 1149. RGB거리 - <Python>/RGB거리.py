1cbe3f8f6bff4378286511e1b0c0f85ce46767aa
DANNY <danny9883@naver.com>
2026-04-23 15:52:15 +0900

    [Silver I] Title: RGB거리, Time: 32 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/[Silver I] 1149. RGB거리 - <Python>/RGB거리.py
+++ 백준/Silver/[Silver I] 1149. RGB거리 - <Python>/RGB거리.py
@@ -1,12 +1,32 @@
-<html>
-<head><title>504 Gateway Time-out</title></head>
-<body>
-<center><h1>504 Gateway Time-out</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+import sys
+input=sys.stdin.readline
+
+
+n = int(input())
+cost = []
+
+for _ in range(n):
+  r, g, b = map(int,input().split())
+  cost.append([r,g,b])
+
+dp = [[0] * 3 for _ in range(n)]
+
+for i in range(3):
+  dp[0][i] = cost[0][i]
+
+
+for i in range(1,n):
+  dp[i][0] = min(dp[i-1][1],dp[i-1][2]) + cost[i][0]
+  dp[i][1] = min(dp[i-1][0],dp[i-1][2]) + cost[i][1]
+  dp[i][2] = min(dp[i-1][1],dp[i-1][0]) + cost[i][2]
+
+
+print(min(dp[n-1]))
+
+
+
+
+
+
+
+
