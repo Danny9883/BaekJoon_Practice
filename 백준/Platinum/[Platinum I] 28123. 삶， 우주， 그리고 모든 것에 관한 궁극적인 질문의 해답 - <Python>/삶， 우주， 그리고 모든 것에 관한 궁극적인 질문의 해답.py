0e89ef498d2a64d60c56ca7296167878dfa04731
DANNY <danny9883@naver.com>
2026-04-27 17:46:04 +0900

    [Platinum I] Title: 삶, 우주, 그리고 모든 것에 관한 궁극적인 질문의 해답, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Platinum/[Platinum I] 28123. 삶， 우주， 그리고 모든 것에 관한 궁극적인 질문의 해답 - <Python>/삶， 우주， 그리고 모든 것에 관한 궁극적인 질문의 해답.py
@@ -0,0 +1,14 @@
+import sys
+input=sys.stdin.readline
+
+
+
+
+n, k, x = map(int,input().split())
+
+if x in [1]:
+  print(n - 3*(k-1))
+elif x in [2,3,4]:
+  print(n - 3*(k-1) - 1)
+elif x in [5,6,7,8,9]:
+  print(n - 3 *(k-1) - 2)
